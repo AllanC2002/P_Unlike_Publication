fee20cc86e22c921bf6b8956435a0a280afa2b4f
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-29 21:49:19 -0500

    Add: MongoDB connection and unlike functionality

---
+++ connection/mongo.cs
@@ -0,0 +1,21 @@
+using MongoDB.Driver;
+using System;
+
+namespace UnlikeService.Connection
+{
+    public class Mongo
+    {
+        public static IMongoDatabase MongoConnection()
+        {
+            var host = Environment.GetEnvironmentVariable("MONGO_HOSTIP");
+            var port = Environment.GetEnvironmentVariable("MONGO_PORT");
+            var user = Environment.GetEnvironmentVariable("MONGO_USER");
+            var pass = Environment.GetEnvironmentVariable("MONGO_PASSWORD");
+            var dbName = Environment.GetEnvironmentVariable("MONGO_DB");
+
+            var uri = $"mongodb://{user}:{pass}@{host}:{port}/?authSource=admin";
+            var client = new MongoClient(uri);
+            return client.GetDatabase(dbName);
+        }
+    }
+}
